e5c03299866cfc26a53b2bb8a930618a6e237d0f
Adam Carlile <adam@benchmedia.co.uk>
2010-08-03 00:57:47 +0100

    Added twitter HTML binned old twitter class

--- app/models/twitter.rb
+++
@@ -1,9 +0,0 @@
-class Twitter
-  include HTTParty
-  basic_auth 'frozenproduce', 'njh9x2'
-  
-  def self.twitter_feed
-    get('http://www.twitter.com/statuses/user_timeline/lmsnowsports.json')
-  end
-  
-end
